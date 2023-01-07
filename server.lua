ceca3c158b955488975c57bfa25b380d6248248a
porterslug <103340263+porterslug@users.noreply.github.com>
2023-01-07 18:34:36 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -19,10 +19,11 @@ AddEventHandler('esx:modelChanged', function(id)
 	TriggerClientEvent('esx:modelChanged', id)
 end)
 
-ESX.RegisterUsableItem('pistol_ammo_box', function(source)
+-- Enter as many items as you want - dont forget you need each item as picture and in your items database
+ESX.RegisterUsableItem('pistol_ammo_box', function(source) 	-- put here the item that should be splitted in several items
 	local xPlayer  = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('pistol_ammo_box', 1)
-	xPlayer.addInventoryItem('pistol_clip', 2)
+	xPlayer.removeInventoryItem('pistol_ammo_box', 1)	-- put here your item again - here the number (1) is for the quantity should removed from your inventory
+	xPlayer.addInventoryItem('pistol_clip', 2)		-- put here your item you would receive - here the number (2) is for the quantity you put in your inventory 
 end)
 
 ESX.RegisterUsableItem('smg_ammo_box', function(source)
